c737eeda770032858b3b7a9451746ea74eaedcf9
Srinivas <ssubramaniyan@algosoftware.io>
2026-06-17 17:32:36 +0530

    Remove debug print statements from MainConsumer connect and disconnect methods

--- chat/consumers.py
+++ chat/consumers.py
@@ -4,10 +4,7 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 class MainConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        print("Websocket Connected")
-
         self.user = self.scope['user']
-        print(self.user)
 
         self.personal_group = f"user_{self.user.id}"
 
@@ -18,17 +15,12 @@ class MainConsumer(AsyncWebsocketConsumer):
 
         await self.accept()
 
-        print(f"{self.user.username} connected — {self.channel_name}")
-
     async def disconnect(self, close_code):
         if hasattr(self, 'personal_group'):
             await self.channel_layer.group_discard(
                 self.personal_group,
                 self.channel_name
             )
-
-        print("Websocket Disconnected")
-        print(f"{self.user.username} disconnected — {self.channel_name}")
 
     async def receive(self, text_data):
         pass
